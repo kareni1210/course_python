ff5b258442b1957c455a7fbfead1e3aadce63325
kareni1210 <idalynava1210@gmail.com>
2024-03-04 12:11:08 -0600

    ex0 and ex1 done

--- class/e0_comments.py
+++ class/e0_comments.py
@@ -1,10 +1,28 @@
-a = 5
+"""
+Excercise 0
+From the file called e0_comments
+Make examples of the different comments we can use in python, place
+teh comment in the correct section of code
+"""
 
+a = 5  # (inline comment) this is an instance of an object, define a variable
+"""
+This is a block comment
+we can add a lot of lines
+"""
 
 def f(x):
-    return x * x
+    """
+    this is a docstring
+    A docstring explains what a function does
+    :param x:
+    :return:
+    """
+    return x * x  # Return the square of x
 
 
 for item in 'python':
+    # An indented comment
     if item == 'p':
+        # Another indented comment 
         print('Found!')
--- class/e1_helloworld.py
+++ class/e1_helloworld.py
@@ -1,20 +1,20 @@
 # Instructions: print out the following sentences, the sentence must show as the comments
 
 # "Once Charles Dickens said: "It was the best of times, it was the worst of times""
-
+print('\n"Once Charles Dickens said: "It was the best of the times, it was worst of times""', end='\n\n')
 
 
 # 5    7    9    11
-
+print(5, 7, 9, 11, sep='\t', end='\n\n')
 
 """
 Actuarial
 Science
 """
-
+print('Actuarial', 'Science', sep='\n', end='\n\n')
 
 # With just three '-' print out five '-'
-
+print('-', '-', '-', sep='-', end='\n\n')
 
 # Hello world (this is the first program that all programmers do)
-
+print('Hello world')
