84367189f957f0b629f01af0dc7698204a2075c1
kareni1210 <idalynava1210@gmail.com>
2024-03-04 12:57:59 -0600

    exercise 2 done

---
+++ class/e2_swapping_variables.py
@@ -0,0 +1,18 @@
+# Way 1
+a = 1
+b = 2
+c = b
+b = a
+a = c
+print('El valor de a es :', a)
+print('El valor de b es:', b)
+
+# Way 2
+a = 1
+b = 2
+c = a
+a = b
+b = c
+print('El valor de a es :', a)
+print('El valor de b es:', b)
+
